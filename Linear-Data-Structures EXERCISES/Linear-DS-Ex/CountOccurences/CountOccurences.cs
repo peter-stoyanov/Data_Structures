5a0ba47b3a05d89d1ba6f9950dda6a8c0df82233
Petar Stoyanov <pesho.stoyanov@gmail.com>
2017-05-26 20:27:52 +0300

    solution to problems 4 and 5

---
+++ Linear-Data-Structures EXERCISES/Linear-DS-Ex/CountOccurences/CountOccurences.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOccurences
+{
+    class CountOccurences
+    {
+        static void Main(string[] args)
+        {
+            //3 4 4 2 3 3 4 3 2	     2 -> 2 times
+
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            var converted = numbers.ConvertAll(x => new { value = x, repeated = numbers.FindAll(y => y == x).Count });
+
+            foreach (var i in converted.Distinct())
+            {
+                if (i.repeated >= 1)
+                {
+                    Console.WriteLine($"{i.value} -> {i.repeated} times");
+                }
+            }
+
+        }
+    }
+}
---
+++ Linear-Data-Structures EXERCISES/Linear-DS-Ex/RemoveOddOccurences/RemoveOddOccurences.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveOddOccurences
+{
+    class RemoveOddOccurences
+    {
+        static void Main(string[] args)
+        {
+            //1 2 3 4 1	1 1
+
+            var numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToList();
+
+            var converted = numbers.ConvertAll(x => new { value = x, odd = numbers.FindAll(y => y == x).Count % 2 != 0 });
+
+            foreach (var i in converted)
+            {
+                if (i.odd == false)
+                {
+                    Console.Write($"{i.value} ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
